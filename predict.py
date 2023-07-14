b8b83074cbf093e51adbe6fd6ddb32f312f240c5
Jerome Hardaway <jhardaway@microsoft.com>
2023-07-13 20:45:52 -0400

    added two utility functions: round_series() to round the values in a series to the nearest whole number, and fit_arima() to fit the ARIMA model and make predictions for a given series; introduced a variable csv_file_path to store the path to the CSV file;added a check to limit the number of columns processed to a maximum of 6. This ensures that if your DataFrame has more than 6 columns, only the first 6 will be considered; added an output_file_path variable to store the path for the predictions CSV file. You can change the value to the desired output file path.

--- predict.py
+++ predict.py
@@ -1,23 +1,36 @@
-# Import necessary libraries
 import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
 from sklearn.metrics import mean_squared_error
 
-# Assume that df is your DataFrame and 'number1' is the column with the numbers
-df = pd.read_csv('pathto.csv')  # replace with your actual csv file
+# Function to round the values in a series to the nearest whole number
+def round_series(series):
+    return series.apply(round)
+
+# Function to fit ARIMA model and make predictions for a series
+def fit_arima(series, order=(5, 1, 0), forecast_steps=3):
+    model = ARIMA(series, order=order)
+    model_fit = model.fit()
+    output = model_fit.forecast(steps=forecast_steps)
+    predictions = round_series(output)
+    return predictions
+
+# Read the CSV file
+csv_file_path = 'pathto.csv'  # replace with your actual csv file path
+df = pd.read_csv(csv_file_path)
 
 # Prepare a DataFrame to store the predictions
 predictions = pd.DataFrame()
 
 # Fit model and make predictions for each column
-for i in range(6):
+num_columns = min(6, df.shape[1])  # Limit to a maximum of 6 columns
+for i in range(num_columns):
     series = df.iloc[:, i]
-    model = ARIMA(series, order=(5,1,0))
-    model_fit = model.fit()
-    output = model_fit.forecast(steps=3)
-    yhat = output.apply(round)  # round to the nearest whole number
-    predictions[f'Predicted future values for column {i+1}'] = yhat
+    column_name = f'Column {i+1}'
+    predicted_values = fit_arima(series)
+    predictions[f'Predicted future values for {column_name}'] = predicted_values
 
 # Print and save the predictions
 print(predictions)
-predictions.to_csv('predictions.csv', index=False)
+output_file_path = 'predictions.csv'
+predictions.to_csv(output_file_path, index=False)
+print(f"Predictions saved to '{output_file_path}'")
